ee3f6b994dbe285f710e1326689c0a61af2b7e7b
Nguyen Hoai Nam <nama1k62cbh@gmail.com>
2026-05-05 17:08:19 +0700

    Design base UI cho login, register

---
+++ src/components/auth/login.tsx
@@ -0,0 +1,61 @@
+'use client'
+
+import React from 'react';
+import type { FormProps } from 'antd';
+import { Button, Checkbox, Form, Input } from 'antd';
+
+const Login = () => {
+    type FieldType = {
+        username?: string;
+        password?: string;
+        name?: string;
+    };
+
+    const onFinish: FormProps<FieldType>['onFinish'] = (values) => {
+        console.log('Success:', values);
+    };
+
+    const onFinishFailed: FormProps<FieldType>['onFinishFailed'] = (errorInfo) => {
+        console.log('Failed:', errorInfo);
+    };
+
+    return (
+        <>
+            <Form
+                name="basic"
+                labelCol={{ span: 8 }}
+                wrapperCol={{ span: 16 }}
+                style={{ maxWidth: 600 }}
+                initialValues={{ remember: true }}
+                onFinish={onFinish}
+                autoComplete="off"
+            >
+                <Form.Item<FieldType>
+                    label="Username"
+                    name="username"
+                    rules={[{ required: true, message: 'Please input!' }]}
+                >
+                    <Input />
+                </Form.Item>
+
+                <Form.Item<FieldType>
+                    label="Password"
+                    name="password"
+                    rules={[{ required: true, message: 'Please input!' }]}
+                >
+                    <Input.Password />
+                </Form.Item>
+
+                {/* Thêm offset: 8 ở đây để đẩy Button sang phải */}
+                <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                    <Button type="primary" htmlType="submit">
+                        Submit
+                    </Button>
+                </Form.Item>
+            </Form>
+        </>
+    )
+}
+
+export default Login;
+
