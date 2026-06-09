dd8fb9fca5d517ef3d64d1ad3fda2a7d084f3671
Nguyen Hoai Nam <nama1k62cbh@gmail.com>
2026-06-09 17:22:52 +0700

    feat(auth): integrate login with backend, handle custom errors and session payload

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,8 @@
 import NextAuth from "next-auth"
 import Credentials from "next-auth/providers/credentials"
+import { InactiveAccountError, InvalidEmailPasswordError } from "./utils/errors";
+import { sendRequest } from "./utils/api";
+import { IUser } from "./types/next-auth";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
@@ -7,36 +10,63 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       // You can specify which fields should be submitted, by adding keys to the `credentials` object.
       // e.g. domain, username, password, 2FA token, etc.
       credentials: {
-        email: {},
+        username: {},
         password: {},
       },
       authorize: async (credentials) => {
-        console.log("check credentials");
-        let user = null;
+        const response = await sendRequest<IBackendRes<ILogin>>({
+          method: "POST",
+          url: "http://localhost:8080/api/v1/auth/login",
+          body: {
+            ...credentials
+          }
+        });
 
         // Call Backend to hash password and verify if the user exist
 
-        user = {
-          _id: "123",
-          username: "123",
-          email: "123",
-          isVerify: "123",
-          type: "123",
-          role: "123",
-        };
-
-        if (!user) {
-          // No user found, so this is their first attempt to login
-          // Optionally, this is also the place you could do a user registration
-          throw new Error("Invalid credentials.")
+        if (response.statusCode === 201) {
+          // return user object with their profile data
+          // console.log({
+          //   _id: response.data?.user._id,
+          //   name: response.data?.user.name,
+          //   email: response.data?.user.email,
+          //   access_token: response.data?.acess_token,
+          // })
+          return {
+            _id: response.data?.user._id,
+            name: response.data?.user.name,
+            email: response.data?.user.email,
+            access_token: response.data?.access_token,
+          };
+        } else if (+response.statusCode === 401) { // Sai mật khẩu 401
+          throw new InvalidEmailPasswordError();
+        } else if (+response.statusCode === 400) { // Tài khoản chưa kích hoạt
+          throw new InactiveAccountError();
+        } else {
+          throw new Error("Internal Sever Error");
         }
-
-        // return user object with their profile data
-        return user
       },
     }),
   ],
   pages: {
     signIn: "/auth/login"
-  }
+  },
+  //  By default, the `id` property does not exist on `token` or `session`. See the [TypeScript](https://authjs.dev/getting-started/typescript) on how to add it.
+  callbacks: {
+    jwt({ token, user }) {
+      if (user) { // User is available during sign-in
+        token.user = (user as IUser);
+      }
+      return token
+    },
+    session({ session, token }) {
+      (session.user as IUser) = token.user;
+      return session
+    },
+    authorized: async ({auth}) => {
+      // Logged in users are authenticated
+      // otherwise redirect to login page
+      return !!auth;
+    }
+  },
 })
