e3690579efe00fd852afef7f375a68c7d1520b70
Nguyen Hoai Nam <nama1k62cbh@gmail.com>
2026-06-10 11:34:11 +0700

    feat: add verify account page and update register flow

--- src/components/auth/verify.tsx
+++ src/components/auth/verify.tsx
@@ -1,30 +1,33 @@
 'use client'
 import React from 'react';
-import { Button, Col, Divider, Form, Input, notification, Row } from 'antd';
+import { Button, Col, Divider, Form, Input, message, notification, Row } from 'antd';
 import { ArrowLeftOutlined } from '@ant-design/icons';
 import Link from 'next/link';
 import { sendRequest } from '@/utils/api';
 import { useRouter } from 'next/navigation';
 
-const Register = () => {
+const Verify = (props: any) => {
+    const {id} = props;
+
     const router = useRouter();
 
     const onFinish = async (values: any) => {
-        const {email, password, name} = values;
+        const {_id, code} = values;
         const res = await sendRequest<IBackendRes<any>>({
-            url: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/auth/register`,
+            url: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/auth/check-code`,
             method: "POST",
             body: {
-                email, password, name
+                _id, code
             }
         })
         console.log(">>> check res: ", res);
         if (res?.data) {
-            router.push(`/verify/${res?.data?._id}`);
+            message.info("Kích hoạt tài khoản thành công!")
+            router.push(`/auth/login`);
         }
         else {
             notification.error({
-                message: "Register error",
+                message: "Verify error",
                 description: res?.message,
             })
         }
@@ -39,7 +42,7 @@ const Register = () => {
                     border: "1px solid #ccc",
                     borderRadius: "5px"
                 }}>
-                    <legend>Đăng Ký Tài Khoản</legend>
+                    <legend>Kích hoạt tài khoản</legend>
                     <Form
                         name="basic"
                         onFinish={onFinish}
@@ -47,36 +50,29 @@ const Register = () => {
                         layout='vertical'
                     >
                         <Form.Item
-                            label="Email"
-                            name="email"
-                            rules={[
-                                {
-                                    required: true,
-                                    message: 'Please input your email!',
-                                },
-                            ]}
+                            label="Id"
+                            name="_id"
+                            initialValue={id}
+                            hidden
                         >
-                            <Input />
+                            <Input disabled />
                         </Form.Item>
 
+                        <div>
+                            Mã code đã được gửi tới email của bạn. Vui lòng kiểm tra email!
+                        </div>
+                        <Divider/>
                         <Form.Item
-                            label="Password"
-                            name="password"
+                            label="Code"
+                            name="code"
                             rules={[
                                 {
                                     required: true,
-                                    message: 'Please input your password!',
+                                    message: 'Please input your code!',
                                 },
                             ]}
                         >
                             <Input.Password />
-                        </Form.Item>
-
-                        <Form.Item
-                            label="Name"
-                            name="name"
-                        >
-                            <Input />
                         </Form.Item>
 
                         <Form.Item
@@ -97,6 +93,6 @@ const Register = () => {
         </Row>
 
     )
-}
+};
 
-export default Register;
+export default Verify;
